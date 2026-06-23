354703f6b4a49c5f1bde99eac0f2c103500cb404
dyssi <torbotijger@proton.me>
2026-06-23 15:01:19 +0200

    Init small projects page with links to online repo's

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,51 @@
 export default function Projects() {
   return (
     <>
-      <main className="rounded-2xl mb-1 p-2.5 hover:bg-gray-100">
-        <a href="https://blog.dyslexic-goegrapher.be">
-          blog.dyslexic-goegrapher.be {" ›"}
-        </a>
+      <main className="rounded-2xl mb-1 p-2.5 ">
+        <ul>
+          <li className="hover:bg-gray-100">
+            <a
+              className="inline-flex items-center gap-2"
+              href="https://codeberg.org/dyslexic-goegrapher"
+            >
+              <img
+                className="h-4 w-4"
+                src="https://codeberg.org/assets/img/favicon.png"
+                alt=""
+                aria-hidden="true"
+              />
+              <span>https://codeberg.org/dyslexic-goegrapher</span>
+            </a>
+          </li>
+          <li className="hover:bg-gray-100">
+            <a
+              className="inline-flex items-center gap-2"
+              href="https://github.com/Dyslexic-Goegrapher"
+            >
+              <img
+                className="h-4 w-4"
+                src="https://github.com/favicon.ico"
+                alt=""
+                aria-hidden="true"
+              />
+              <span>https://github.com/Dyslexic-Goegrapher</span>
+            </a>
+          </li>
+          <li className="hover:bg-gray-100">
+            <a
+              className="inline-flex items-center gap-2"
+              href="https://tangled.org/dyslexic-goegrapher.be"
+            >
+              <img
+                className="h-4 w-4"
+                src="https://tangled.org/static/logos/dolly.ico"
+                alt=""
+                aria-hidden="true"
+              />
+              <span>https://tangled.org/dyslexic-goegrapher.be</span>
+            </a>
+          </li>
+        </ul>
       </main>
     </>
   );
